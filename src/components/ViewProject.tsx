b765790f6444c4fc53ee2b9576e4fa06fe940224
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-02-13 13:24:49 +0100

    Refactor counters into Counter component

--- src/components/ViewProject.tsx
+++ src/components/ViewProject.tsx
@@ -1,64 +1,56 @@
 import type { Project } from "@prisma/client";
-import { useCallback } from "react";
-import { useRefetch } from "../pages/[projectId]";
+import { useMemo } from "react";
 import { api } from "../utils/api";
 import { PlusIcon, MinusIcon } from "./util";
 
 export const ProjectView = ({ project }: { project: Project }) => {
-  const incrementPindMutation = api.project.increasePindCount.useMutation();
-  const decrementPindMutation = api.project.decreasePindCount.useMutation();
-  const incrementOmgangMutation = api.project.increaseOmgangCount.useMutation();
-  const decrementOmgangMutation = api.project.decreaseOmgangCount.useMutation();
-  const disabled =
-    incrementPindMutation.isLoading ||
-    decrementPindMutation.isLoading ||
-    incrementOmgangMutation.isLoading ||
-    decrementOmgangMutation.isLoading;
-
   return (
     <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4">
       {/* <h2 className="my-4 mx-1 text-4xl">Projekter</h2> */}
 
       <h3 className="text-2xl font-bold">{project.name}</h3>
       <div className="grid grid-cols-2 gap-2">
-        <div className="grid grid-flow-col grid-cols-2 grid-rows-2 items-center gap-2">
-          <div>{project.pindCount}</div>
-          <span>Pinde</span>
-          <CounterButton
-            value={project.pindCount}
-            target="Pind"
-            projectId={project.id}
-            type="increase"
-          />
-          <CounterButton
-            value={project.pindCount}
-            target="Pind"
-            projectId={project.id}
-            type="decrease"
-          />
-        </div>
-        <div className="grid grid-flow-col grid-cols-2 grid-rows-2 items-center gap-2">
-          <div>{project.omgangCount}</div>
-          <span>Omgange</span>
-
-          <CounterButton
-            value={project.omgangCount}
-            target="Omgang"
-            projectId={project.id}
-            type="increase"
-          />
-          <CounterButton
-            value={project.omgangCount}
-            target="Omgang"
-            projectId={project.id}
-            type="decrease"
-          />
-        </div>
+        <Counter project={project} target="Pind" label="Pinde" />
+        <Counter project={project} target="Omgang" label="Omgange" />
       </div>
       <p className="text-lg">{project.description}</p>
     </div>
   );
 };
+
+function Counter({
+  project,
+  target,
+  label,
+}: {
+  project: Project;
+  target: "Pind" | "Omgang";
+  label: string;
+}) {
+  const targetKey = useMemo(
+    () => `${target.toLowerCase() as "pind" | "omgang"}Count` as const,
+    [target]
+  );
+
+  return (
+    <div className="grid grid-flow-col grid-cols-2 grid-rows-2 items-center gap-2">
+      <div>{project[targetKey]}</div>
+      <span className="text-white/60">{label}</span>
+      <CounterButton
+        value={project[targetKey]}
+        target={target}
+        projectId={project.id}
+        type="increase"
+      />
+      <CounterButton
+        value={project[targetKey]}
+        target={target}
+        projectId={project.id}
+        type="decrease"
+      />
+    </div>
+  );
+}
 
 function CounterButton({
   value,
@@ -71,18 +63,14 @@ function CounterButton({
   target: "Pind" | "Omgang";
   projectId: string;
 }) {
-  const mutation = api.project[`${type}${target}Count`].useMutation();
-  const refetch = useRefetch();
-  const disabled =
-    mutation.status === "loading" || (type === "decrease" && value < 1);
-
-  const performActionAndRefresh = useCallback(
-    async function performActionAndRefresh(promise: Promise<unknown>) {
-      await promise;
-      refetch();
+  const ctx = api.useContext();
+  const mutation = api.project[`${type}${target}Count`].useMutation({
+    onSuccess: async () => {
+      await ctx.project.invalidate();
     },
-    [refetch]
-  );
+  });
+
+  const disabled = type === "decrease" && value < 1;
 
   return (
     <button
@@ -92,11 +80,9 @@ function CounterButton({
           : "bg-purple-200/20 hover:bg-white/30"
       }`}
       onClick={() =>
-        void performActionAndRefresh(
-          mutation.mutateAsync({
-            projectId,
-          })
-        )
+        void mutation.mutateAsync({
+          projectId,
+        })
       }
       disabled={disabled}
     >
