97880aa617861de7514b73016c8225759ae06ca3
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2023-02-15 13:38:44 +0100

    Format code

--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -1,30 +1,33 @@
-import { useSession } from "next-auth/react"
-import { api } from "../utils/api"
+import { useSession } from "next-auth/react";
+import { api } from "../utils/api";
 
 const ProjectForm = () => {
-  const session = useSession()
-  const mut = api.project.createProject.useMutation()
+  const session = useSession();
+  const mut = api.project.createProject.useMutation();
   function createProject(evt: React.FormEvent<HTMLFormElement>) {
-    const userId = session.data?.user.id
-    if (!userId) return
+    const userId = session.data?.user.id;
+    if (!userId) return;
 
-    evt.preventDefault()
-    const form = evt.target as HTMLFormElement
-    const name = form.elements.namedItem("name") as HTMLInputElement
+    evt.preventDefault();
+    const form = evt.target as HTMLFormElement;
+    const name = form.elements.namedItem("name") as HTMLInputElement;
     mut.mutate({
       name: name.value,
       description: "test",
-      ownerId: session.data.user.id
-    })
+      ownerId: session.data.user.id,
+    });
   }
-  return <div>
-    <h2>Opret project</h2>
-    <form onSubmit={createProject} >
-      <label htmlFor="name">Navn
-        <input type="text" name="name" id="name" />
-      </label>
-    </form>
-  </div>
-}
+  return (
+    <div>
+      <h2>Opret project</h2>
+      <form onSubmit={createProject}>
+        <label htmlFor="name">
+          Navn
+          <input type="text" name="name" id="name" />
+        </label>
+      </form>
+    </div>
+  );
+};
 
-export default ProjectForm
+export default ProjectForm;
